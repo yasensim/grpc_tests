bcdb86ecffab39afa7a12b22787665a40da506e9
Yasen Simeonov <yasen_simeonov@intuit.com>
2021-12-24 16:14:24 -0800

    blog with mongo

--- generate.sh
+++ generate.sh
@@ -1,3 +1,8 @@
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./greet/greetpb/greet.proto
 
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./calculator/calculatorpb/calculator.proto 
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative blog/blogpb/blog.proto 
+
+/Users/ysimeonov/mongodb-macos-x86_64-5.0.5/bin/mongod --dbpath data/db/
+
